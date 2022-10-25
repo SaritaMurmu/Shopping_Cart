857c06203595cdc4554af97ab7f002f0df112c34
SaritaMurmu <saritamurmu28@gmail.com>
2022-10-25 17:20:37 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Navbar from './Common/Navbar';
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+// import Footer from './Common/Footer';
+
+function App() {
+  return (
+    <div>
+       <Router>
+        <Navbar/>
+        <div style={{height:100}}></div>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+        {/* <Footer/> */}
+       </Router>
+    </div>
+  );
+}
+
+export default App;
